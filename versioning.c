e220a288f7249db493c15ba2d13846154ce0cff2
Mikhail Titov <mlt@gmx.us>
2017-08-31 15:10:19 -0500

    fixup! Versioning using oid

--- versioning.c
+++ versioning.c
@@ -6,8 +6,6 @@
  *
  * -------------------------------------------------------------------------
  */
-#define __STDC_WANT_LIB_EXT1__ 1
-#include <string.h>
 #include "postgres.h"
 
 #include <math.h>
@@ -598,7 +596,7 @@ fill_versioning_hash_entry(VersioningHashEntry *hash_entry,
 		oldcontext = MemoryContextSwitchTo(TopMemoryContext);
 
 		hash_entry->history_relid = RelationGetRelid(history_relation);
-		strncpy_s(hash_entry->history_relname, NAMEDATALEN, NameStr(history_relation->rd_rel->relname), NAMEDATALEN);
+		strncpy(hash_entry->history_relname, NameStr(history_relation->rd_rel->relname), NAMEDATALEN);
 		hash_entry->tupdesc = CreateTupleDescCopyConstr(tupdesc);
 		hash_entry->history_tupdesc = CreateTupleDescCopyConstr(history_tupdesc);
 
